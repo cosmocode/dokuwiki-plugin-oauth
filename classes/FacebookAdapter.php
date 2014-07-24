6d4f53b4119c691288ec1850f8b12e3711b73ce2
Andreas Gohr <gohr@cosmocode.de>
2014-07-24 16:46:11 +0200

    started Facebook-Adapter (not working yet)

---
+++ classes/FacebookAdapter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OAuth\Plugin;
+
+
+
+use OAuth\OAuth2\Service\Facebook;
+
+class FacebookAdapter extends AbstractAdapter {
+
+    /**
+     * Retrieve the user's data
+     *
+     * The array needs to contain at least 'user', 'email', 'name' and optional 'grps'
+     *
+     * @return array
+     */
+    public function getUser() {
+        $JSON = new \JSON(JSON_LOOSE_TYPE);
+        $data = array();
+
+        $result = $JSON->decode($this->oAuth->request('/me'));
+
+        dbg($result);
+
+
+        $data['user'] = $result['name'];
+        $data['name'] = $result['name'];
+        $data['mail'] = $result['email'];
+
+        return $data;
+    }
+
+    /**
+     * Access to user and his email addresses
+     *
+     * @return array
+     */
+    public function getScope() {
+        return array(Facebook::SCOPE_EMAIL);
+    }
+
+}
