913e6b23a0a56ce87ff7a82bb34b90d26586c28d
Andreas Gohr <andi@splitbrain.org>
2014-07-26 09:44:54 +0200

    added Google (untested)
    don't have Internet right now

---
+++ classes/GoogleAdapter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OAuth\Plugin;
+
+use OAuth\OAuth2\Service\Google;
+
+class GoogleAdapter extends AbstractAdapter {
+
+    /**
+     * Retrieve the user's data
+     *
+     * The array needs to contain at least 'user', 'email', 'name' and optional 'grps'
+     *
+     * @return array
+     */
+    public function getUser() {
+        $JSON = new \JSON(JSON_LOOSE_TYPE);
+        $data = array();
+
+        $result = $JSON->decode($this->oAuth->request('https://www.googleapis.com/oauth2/v1/userinfo'));
+        $data['user'] = $result['login'];
+        $data['name'] = $result['name'];
+        $data['mail'] = $result['email'];
+
+        return $data;
+    }
+
+    /**
+     * Access to user and his email addresses
+     *
+     * @return array
+     */
+    public function getScope() {
+        return array(Google::SCOPE_USERINFO_EMAIL, Google::SCOPE_USERINFO_PROFILE);
+    }
+
+}
