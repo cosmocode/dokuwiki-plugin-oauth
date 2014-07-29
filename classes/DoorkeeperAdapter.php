afbdd6c2576c2ed59800955c20d3300a3f8de1b0
Andreas Gohr <gohr@cosmocode.de>
2014-07-29 12:38:22 +0200

    added some more docs to the example implementation

--- classes/DoorkeeperAdapter.php
+++ classes/DoorkeeperAdapter.php
@@ -2,7 +2,19 @@
 
 namespace OAuth\Plugin;
 
-
+/**
+ * Class DoorkeeperAdapter
+ *
+ * This is an example on how to implement your own adapter for making DokuWiki login against
+ * a custom oAuth provider. The used Generic Service backend expects the authorization and
+ * token endpoints to be configured in the DokuWiki backend.
+ *
+ * Your custom API to access user data has to be implemented in the getUser function. The one here
+ * is setup to work with the demo setup of the "Doorkeeper" ruby gem.
+ *
+ * @link https://github.com/doorkeeper-gem/doorkeeper
+ * @package OAuth\Plugin
+ */
 class DoorkeeperAdapter extends AbstractAdapter {
 
     /**
@@ -16,15 +28,22 @@ class DoorkeeperAdapter extends AbstractAdapter {
         $JSON = new \JSON(JSON_LOOSE_TYPE);
         $data = array();
 
+        /** var OAuth\OAuth2\Service\Generic $this->oAuth */
         $result = $JSON->decode($this->oAuth->request('https://doorkeeper-provider.herokuapp.com/api/v1/me.json'));
 
         $data['user'] = 'doorkeeper-'.$result['id'];
-        //$data['name'] = $result['name'];
+        $data['name'] = 'doorkeeper-'.$result['id'];
         $data['mail'] = $result['email'];
 
         return $data;
     }
 
+    /**
+     * We make use of the "Generic" oAuth 2 Service as defined in
+     * phpoauthlib/src/OAuth/OAuth2/Service/Generic.php
+     *
+     * @return string
+     */
     public function getServiceName() {
         return 'Generic';
     }
