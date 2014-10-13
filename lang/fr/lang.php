960ab1ee51cc3a244f739057fe41c2663896aad1
Cyrille37 <cyrille@giquello.fr>
2014-10-14 01:26:27 +0200

    French translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * French language file for oauth plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ * @author Cyrille Giquello <cyrille@giquello.fr>
+ */
+
+$lang['emailduplicate'] = 'Cette email est déjà associée à un utilisateur.';
+$lang['loginwith']      = 'Connectez vous avec d´autres services:';
+$lang['authnotenabled'] = 'Le compte associé à cette email n´a pas autorisé la connection avec %s. Connectez-vous à ce compte par un autre moyen pour activer ce service.';
---
+++ lang/fr/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * French language file for oauth plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ * @author Cyrille Giquello <cyrille@giquello.fr>
+ */
+
+
+$lang['info']            = 'URI de redirection (Redirect URI) à utiliser dans la configuration de l´application';
+$lang['facebook-key']    = 'L´App ID de votre <a href="https://developers.facebook.com/apps">application Facebook</a>';
+$lang['facebook-secret'] = 'L´App Secret de votre <a href="https://developers.facebook.com/apps">application Facebook</a>';
+$lang['github-key']      = 'Le Client ID de votre <a href="https://github.com/settings/applications">application Github</a>';
+$lang['github-secret']   = 'Le Client Secret de votre <a href="https://github.com/settings/applications">application Github</a>';
+$lang['google-key']      = 'Le Client ID de votre <a href="https://console.developers.google.com/project">projet Google</a> (voir Credentials Screen)';
+$lang['google-secret']   = 'Le Client Secret de votre <a href="https://console.developers.google.com/project">projet Google</a> (voir Credentials Screen)';
+$lang['yahoo-key']       = 'La Consumer Key de votre <a href="https://developer.apps.yahoo.com/dashboard/createKey.html">application Yahoo</a>';
+$lang['yahoo-secret']    = 'Le Consumer Secret de votre <a href="https://developer.apps.yahoo.com/dashboard/createKey.html">application Yahoo</a>';
+$lang['doorkeeper-key']      = '(Exemple) l´Application ID de votre application Doorkeeper.';
+$lang['doorkeeper-secret']   = '(Exemple) Le Secret de votre application Doorkeeper.';
+$lang['doorkeeper-authurl']  = '(Exemple) Le Authorization Endpoint URL de votre configuration Doorkeeper.';
+$lang['doorkeeper-tokenurl'] = '(Exemple) Le Access Token Endpoint URL de votre configuration Doorkeeper.';
