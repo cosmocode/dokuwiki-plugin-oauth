a8afc8dfc0c58f5b3238a10db0e99caba7c9640e
Ingo Wichmann <iw@linuxhotel.de>
2014-11-17 23:47:19 +0100

    German language frontend translation

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language file for oauth plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['emailduplicate'] = 'Diese Emailadresse ist bereits mit einem anderen Benutzer verknüpft.';
+$lang['loginwith']      = 'Mit anderen Diensten anmelden:';
+$lang['authnotenabled'] = 'Die mit Deinem Benutzerkonto verbundene Emailadresse erlaubt es nicht sich bei %s anzumelden. Bitte melde Dich auf einem anderen Weg an und erlaube es in Deinem Profil.';
---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language file for oauth plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['emailduplicate'] = 'Diese Emailadresse ist bereits mit einem anderen Benutzer verknüpft.';
+$lang['loginwith']      = 'Mit anderen Diensten anmelden:';
+$lang['authnotenabled'] = 'Die mit Ihrem Benutzerkonto verbundene Emailadresse erlaubt es nicht sich bei %s anzumelden. Bitte melden Sie sich auf einem anderen Weg an und erlauben Sie es in Ihrem Profil.';
