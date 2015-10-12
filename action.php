c29c7913bb7a469d53139dda44602a5aed9852d0
Michael Große <grosse@cosmocode.de>
2015-10-12 11:02:47 +0200

    Save page only if there is text to be saved
    Occasionally a page was deleted, i.e. saved with no text, upon relogin
    forced by oauth. It is unclear how this was caused, but this fix should
    prevent the deletion and show the page before editing to the user.

--- action.php
+++ action.php
@@ -46,12 +46,16 @@ class action_plugin_oauth extends DokuWiki_Action_Plugin {
         if (isset($_SESSION[DOKU_COOKIE]['oauth-done']['do']) || !empty($_SESSION[DOKU_COOKIE]['oauth-done']['rev'])){
             global $ACT, $TEXT, $PRE, $SUF, $SUM;
             $ACT = $_SESSION[DOKU_COOKIE]['oauth-done']['do'];
-            if (isset($_SESSION[DOKU_COOKIE]['oauth-done']['wikitext'])) {
+            if (!empty($_SESSION[DOKU_COOKIE]['oauth-done']['wikitext'])) {
                 $TEXT = cleanText($_SESSION[DOKU_COOKIE]['oauth-done']['wikitext']);
                 $PRE = cleanText(substr($_SESSION[DOKU_COOKIE]['oauth-done']['prefix'], 0, -1));
                 $SUF = cleanText($_SESSION[DOKU_COOKIE]['oauth-done']['suffix']);
                 $SUM = $_SESSION[DOKU_COOKIE]['oauth-done']['summary'];
                 $INPUT->post->set('sectok', $_SESSION[DOKU_COOKIE]['oauth-done']['sectok']);
+            }
+            if ($ACT === 'save' && empty($TEXT))
+            {
+                $ACT = 'show';
             }
 
             // resetting INPUT, ->post and ->get
