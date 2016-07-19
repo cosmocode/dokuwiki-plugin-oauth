ac2264144165120994b645f8e68e42ec359e1405
Ian Moroney <ianmoroney@gmail.com>
2016-07-19 17:14:37 +0100

    Update lang.php
    Fixed typo in error message

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -8,7 +8,7 @@
 $lang['emailduplicate'] = 'This email is already associated with another user.';
 $lang['loginwith']      = 'Login with other Services:';
 $lang['authnotenabled'] = 'The account associated with your email address has not enabled logging in with %s. Please login by other means and enable it in your profile.';
-$lang['wrongConfig'] = 'The oAuth plugin has been malconfiguered. Defaulting to local authentication only. Please contact your wiki administrator.';
+$lang['wrongConfig'] = 'The oAuth plugin has been malconfigured. Defaulting to local authentication only. Please contact your wiki administrator.';
 $lang['loginButton'] = 'Login with ';//... i.e. Google (on SingleAuth)
 $lang['rejectedEMail'] = 'Invalid eMail-Account used. Only email accounts from the following domain(s) are allowed: %s!';
 $lang['eMailRestricted'] = '<p id="oauth_email_restricted">Only email accounts from the following domain(s) are allowed: %s</p>';
