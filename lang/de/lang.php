846e85c6f03600ccebf65a38aefcc4188c34a7c4
F. Mueller-Donath <j.felix@mueller-donath.de>
2017-10-02 14:50:43 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['emailduplicate']        = 'Diese Emailadresse ist bereits mit einem anderen Benutzerprofil verknüpft.';
+$lang['loginwith']             = 'Mit anderen Services anmelden:';
+$lang['authnotenabled']        = 'Für das mit dieser Emailadresse verknüpfte Benutzerprofil ist die Anmeldung mit %s nicht erlaubt. Bitte melden Sie sich auf eine andere Art an um Ihr Benutzerprofil dafür zu konfigurieren.';
+$lang['wrongConfig']           = 'Das oAuth Plugin ist fehlerhaft konfiguriert, es können derzeit nur lokale Anmeldedaten verwendet werden. Bitte wenden Sie Sich an den Administrator dieses Wikis.';
+$lang['loginButton']           = 'Anmelden mit';
+$lang['rejectedEMail']         = 'Ungültige Emailadresse. Nur Adressen aus den folgenden Domänen sind erlaubt: %s!';
+$lang['eMailRestricted']       = '<p id="oauth_email_restricted">Nur Adressen aus den folgenden Domänen sind erlaubt: %s!</p>';
+$lang['addUser not possible']  = 'Die Registrierung ist nicht erlaubt oder conf/users.auth.php ist nicht schreibbar. Bitte wenden Sie Sich an den Administrator dieses Wikis.';
+$lang['oauth login failed']    = 'Anmeldung fehlgeschlagen.';
---
+++ lang/de/settings.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['info']                  = 'Weiterleitungs-URI für die Konfiguration der Applikationen';
+$lang['custom-redirectURI']    = 'Folgende benutzerdefinierte Weiterleitungs-URI verwenden';
+$lang['auth0-key']             = 'Client ID Ihrer <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
+$lang['auth0-secret']          = 'Client Geheimcode Ihrer <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
+$lang['auth0-domain']          = 'Domäne Ihrer <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
+$lang['facebook-key']          = 'App-ID Ihrer <a href="https://developers.facebook.com/apps">Facebook-App</a>';
+$lang['facebook-secret']       = 'App-Geheimcode Ihrer <a href="https://developers.facebook.com/apps">Facebook-App</a>';
+$lang['github-key']            = 'Client ID Ihrer <a href="https://github.com/settings/applications">Github Applikation</a>';
+$lang['github-secret']         = 'Client Secret Ihrer <a href="https://github.com/settings/applications">Github Applikation</a>';
+$lang['google-key']            = 'Client-ID Ihres <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
+$lang['google-secret']         = 'Clientschlüssel Ihres <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
+$lang['dataporten-key']        = 'Client ID Ihrer <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
+$lang['dataporten-secret']     = 'Client Geheimhcode Ihrer <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
+$lang['mailRestriction']       = 'Beschränke Anmeldung auf Benutzerprofile dieser Domäne (optional, muss mit <code>@</code> beginnen)';
+$lang['singleService']         = 'Nur einen oAuth Service zulassen (lokale Anmeldung wird deaktiviert!)';
+$lang['singleService_o_']      = 'Alle Services zulassen';
