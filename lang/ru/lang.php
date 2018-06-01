ac368260198836d5158ea43270241658f2ee31e8
NoName <postname9@gmail.com>
2018-06-01 22:55:14 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author NoName <postname9@gmail.com>
+ */
+$lang['loginwith']             = 'Войти через другие Сервисы';
+$lang['loginButton']           = 'Войти через';
+$lang['eMailRestricted']       = '<p id="oauth_email_restricted">Разрешены аккаунты электронных почт только со следующих доменов: %s</p>';
