7fbfe37da22fda89ab15b3853ff4308131d4a68b
Hans Drexler <bedrijven-post@klaan.nl>
2019-03-13 14:35:11 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hans Drexler <bedrijven-post@klaan.nl>
+ */
+$lang['info']                  = 'Redirect URI om te gebruiken voor applicatie configuratie';
+$lang['custom-redirectURI']    = 'Gebruik de volgende custom redirect URI';
