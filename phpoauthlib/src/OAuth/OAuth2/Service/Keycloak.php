a806757683626d392b5a67a8564a105853f7174c
Naoto Kobayashi <pollyanna@misclog.net>
2020-06-14 14:45:49 +0900

    Add Keycloak scope to support Keycloak 10
    Keycloak 10 returns the error "invalid_scope" when a client tries to
    get an access token with no scopes.
    
    Fix it.
    
    Signed-off-by: Naoto Kobayashi <pollyanna@misclog.net>

--- phpoauthlib/src/OAuth/OAuth2/Service/Keycloak.php
+++ phpoauthlib/src/OAuth/OAuth2/Service/Keycloak.php
@@ -13,6 +13,12 @@ use OAuth\Common\Http\Uri\UriInterface;
 
 class Keycloak extends Generic
 {
+    /**
+     * Defined scopes are listed here:
+     * @link https://www.keycloak.org/docs/latest/server_admin/#_client_scopes
+     */
+    const SCOPE_OPENID = 'openid';
+
     protected function getAuthorizationMethod()
     {
         return static::AUTHORIZATION_METHOD_HEADER_BEARER;
