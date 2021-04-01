77457d19bac8f15c19a816ef1b8734baa76eb525
DrMaXX <drmaxxis@gmail.com>
2021-04-01 10:00:34 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author DrMaXX <drmaxxis@gmail.com>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
 $lang['info']                  = 'Weiterleitungs-URI für die Konfiguration der Applikationen';
@@ -18,7 +19,18 @@ $lang['google-key']            = 'Client-ID Ihres <a href="https://console.devel
 $lang['google-secret']         = 'Clientschlüssel Ihres <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
 $lang['dataporten-key']        = 'Client ID Ihrer <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
 $lang['dataporten-secret']     = 'Client Geheimhcode Ihrer <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
+$lang['keycloak-key']          = 'Die Ressourcen-ID Ihrer Keycloak-Anwendung.';
+$lang['keycloak-secret']       = 'Des Secret Ihrer Keycloak-Anwendung.';
+$lang['keycloak-authurl']      = 'Die URL des Autorisierungsendpunkts Ihres Doorkeeper-Setups.';
+$lang['keycloak-tokenurl']     = 'Die Access-Token-Endpunkt-URL Ihres Doorkeeper-Setups.';
+$lang['keycloak-userinfourl']  = 'Die Userinfo-Endpunkt-URL Ihres Keycloak-Setups.';
 $lang['mailRestriction']       = 'Beschränke Anmeldung auf Benutzerprofile dieser Domäne (optional, muss mit <code>@</code> beginnen)';
+$lang['yahoo-key']             = 'Der Consumer Schlüssel Ihrer registrierten <a href="https://developer.apps.yahoo.com/dashboard/createKey.html">Yahoo-Anwendung</a>';
+$lang['yahoo-secret']          = 'Das Consumer Secret Ihrer registrierten <a href="https://developer.apps.yahoo.com/dashboard/createKey.html">Yahoo-Anwendung</a>';
+$lang['doorkeeper-key']        = '(Beispiel) Die Applikations-ID Ihrer registrierten Doorkeeper-Applikation.';
+$lang['doorkeeper-secret']     = '(Beispiel) Das Secret Ihrer registrierten Doorkeeper-Applikation.';
+$lang['doorkeeper-authurl']    = '(Beispiel) Die URL des Autorisierungsendpunkts Ihres Doorkeeper-Setups.';
+$lang['doorkeeper-tokenurl']   = '(Beispiel) Die Access-Token-Endpunkt-URL Ihres Doorkeeper-Setups.';
 $lang['singleService']         = 'Nur einen oAuth Service zulassen (lokale Anmeldung wird deaktiviert!)';
 $lang['singleService_o_']      = 'Alle Services zulassen';
 $lang['register-on-auth']      = 'Authentifizierte Benutzer registrieren, auch wenn Selbstregistrierung in der Hauptkonfiguration nicht erlaubt ist.';
