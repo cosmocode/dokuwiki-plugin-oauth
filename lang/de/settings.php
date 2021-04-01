01d5737b8a09535a70cb13c0f6e1ea8c0e3d8074
inv Alide <inv@li.de>
2021-04-01 09:30:31 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,22 +3,25 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author inv Alide <inv@li.de>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
 $lang['info']                  = 'Weiterleitungs-URI für die Konfiguration der Applikationen';
 $lang['custom-redirectURI']    = 'Folgende benutzerdefinierte Weiterleitungs-URI verwenden';
-$lang['auth0-key']             = 'Client ID Ihrer <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
-$lang['auth0-secret']          = 'Client Geheimcode Ihrer <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
-$lang['auth0-domain']          = 'Domäne Ihrer <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
-$lang['facebook-key']          = 'App-ID Ihrer <a href="https://developers.facebook.com/apps">Facebook-App</a>';
-$lang['facebook-secret']       = 'App-Geheimcode Ihrer <a href="https://developers.facebook.com/apps">Facebook-App</a>';
-$lang['github-key']            = 'Client ID Ihrer <a href="https://github.com/settings/applications">Github Applikation</a>';
-$lang['github-secret']         = 'Client Secret Ihrer <a href="https://github.com/settings/applications">Github Applikation</a>';
-$lang['google-key']            = 'Client-ID Ihres <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
-$lang['google-secret']         = 'Clientschlüssel Ihres <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
-$lang['dataporten-key']        = 'Client ID Ihrer <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
-$lang['dataporten-secret']     = 'Client Geheimhcode Ihrer <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
+$lang['auth0-key']             = 'Client ID deiner <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
+$lang['auth0-secret']          = 'Client Geheimcode deiner <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
+$lang['auth0-domain']          = 'Domäne deiner <a href="https://manage.auth0.com/#/applications">Auth0 Applikation</a>';
+$lang['facebook-key']          = 'App-ID deiner <a href="https://developers.facebook.com/apps">Facebook-App</a>';
+$lang['facebook-secret']       = 'App-Geheimcode deiner <a href="https://developers.facebook.com/apps">Facebook-App</a>';
+$lang['github-key']            = 'Client ID deiner <a href="https://github.com/settings/applications">Github Applikation</a>';
+$lang['github-secret']         = 'Client Secret deiner <a href="https://github.com/settings/applications">Github Applikation</a>';
+$lang['google-key']            = 'Client-ID deines <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
+$lang['google-secret']         = 'Clientschlüssel deines <a href="https://console.developers.google.com/project">Google Projects</a> (siehe "APIs & Dienste / Zugangsdaten")';
+$lang['dataporten-key']        = 'Client ID deiner <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
+$lang['dataporten-secret']     = 'Client Geheimhcode deiner <a href="https://dashboard.dataporten.no">Dataporten Applikation</a>';
+$lang['keycloak-key']          = 'Resource ID deiner Keycloak Applikation';
+$lang['keycloak-secret']       = 'Geheimcode deiner Keycloak Applikation';
 $lang['mailRestriction']       = 'Beschränke Anmeldung auf Benutzerprofile dieser Domäne (optional, muss mit <code>@</code> beginnen)';
 $lang['singleService']         = 'Nur einen oAuth Service zulassen (lokale Anmeldung wird deaktiviert!)';
 $lang['singleService_o_']      = 'Alle Services zulassen';
-$lang['register-on-auth']      = 'Authentifizierte Benutzer registrieren, auch wenn Selbstregistrierung in der Hauptkonfiguration nicht erlaubt ist.';
+$lang['register-on-auth']      = 'Registriere authentifizierte Benutzer selbst wenn Selbstregistrierung in der Hauptkonfiguration nicht erlaubt ist.';
