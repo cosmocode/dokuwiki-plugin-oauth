3e5be593f2844bd5cd3f1462bf2800fef9d6a6ac
Andreas Gohr <andi@splitbrain.org>
2021-12-04 00:47:06 +0100

    Re-Added custom metadata class for redirect info
    replaces #95

--- conf/metadata.php
+++ conf/metadata.php
@@ -5,6 +5,8 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 
+
+$meta['info']                = array(\dokuwiki\plugin\oauth\RedirectSetting::class);
 $meta['custom-redirectURI']  = array('string','_caution' => 'warning');
 $meta['mailRestriction']     = array('string','_pattern' => '!^(@[^,@]+(\.[^,@]+)+(,|$))*$!'); // https://regex101.com/r/mG4aL5/3
 $meta['singleService']       = array('onoff');
