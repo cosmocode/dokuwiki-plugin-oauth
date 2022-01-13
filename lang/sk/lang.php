dca744f700def529e59d254420f7005dffbbcc4a
Viktor Kristian <vkristian@gmail.com>
2022-01-13 21:57:46 +0100

    added translation to sk language

---
+++ lang/sk/lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Slovak language file for oauth plugin
+ *
+ * @author Viktor Kristian <vkristian@gmail.com>
+ */
+
+$lang['emailduplicate'] = 'Táto e-mailová adresa je už asociovaná s iným používateľom.';
+$lang['loginwith']      = 'Prihlásiť sa pomocou iných služieb:';
+$lang['authnotenabled'] = 'Používateľský účet asociovaný s vašou e-mailovou adresou nemá povolené prihlasovanie sa pomocou %s. Prosím prihláste sa inou metódou, a vo svojom používateľskom profile povoľte prihlasovanie sa pomocou %s.';
+$lang['wrongConfig'] = 'oAuth plugin nie je správne nakonfigurovaný. Je možné prihlásiť sa len pomocou lokálneho účtu. Prosím kontaktujte administrátora stránky.';
+$lang['loginButton'] = 'Prihlásiť sa pomocou ';//... i.e. Google (on SingleAuth)
+$lang['rejectedEMail'] = 'Použitá nesprávna e-mailová adresa. Povolené sú len e-mailové adresy z nasledujúcich domén: %s!';
+$lang['eMailRestricted'] = 'Povolené sú len e-mailové adresy z nasledujcich domén: %s';
+$lang['noEmail'] = 'Služba %s nám neposkytla vašu e-mailovú adresu. Nevieme vás preto prihlásiť.';
+$lang['addUser not possible'] = 'Samoregistrácia užívateľa je momentálne zakázaná, alebo konfigurácia v conf/users.auth.php nie je zapisovateľná. Prosím požiadajte administrátora stránky, aby vám vytvoril váš účet manuálne.';
+$lang['oauth login failed'] = 'Vaše (opätovné) prihlásenie sa zlyhalo.';
+$lang['generic create error'] = 'Počas vytvárania vášho užívateľského účtu niekde nastala chyba. Prosím skúste to znova neskôr.';
