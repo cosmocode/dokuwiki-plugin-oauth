0ccb80eb7672bacb405cf2de13a80588dd75a860
David 'ilicz' Klementa <ilicz@inmail.cz>
2022-02-13 16:56:09 +0100

    Add translation to cs language

---
+++ lang/cs/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * slovak language file for oauth plugin
+ *
+ * @author David 'ilicz' Klementa <ilicz@inmail.cz>
+ */
+
+
+$lang['info']               = 'Redirect URI konfiguraci aplikací';
+$lang['custom-redirectURI'] = 'Použít vlastní redirect URI';
+$lang['mailRestriction']    = 'Omezit autentizaci na užívatele z této domény (volitelné, musí začínat <code>@</code>)';
+$lang['singleService']      = 'Povolit přihlášení pouze se službou oAuth (zakáže přihlášení lokálními účty!)';
+$lang['singleService_o_']   = 'Povolit všechny služby';
+$lang['register-on-auth']   = 'Zaregistrovat autentizované uživatele i když je samo-registrace zakázaná v nastavení zabezpečení';
