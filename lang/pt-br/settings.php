6341de03f8d3ba286763424350a0843965b10a93
Gilson Caldeira <gilsoncaldeira@gmail.com>
2023-03-29 19:00:16 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Gilson Caldeira <gilsoncaldeira@gmail.com>
+ */
+$lang['info']                  = 'Redirecione o URI para usar ao configurar os aplicativos';
+$lang['custom-redirectURI']    = 'Use o URI de redirecionamento personalizado a seguir';
+$lang['mailRestriction']       = 'Limite a autenticação a usuários deste domínio (opcional, deve começar com um <code>@</code>)';
+$lang['singleService']         = 'Faça login apenas com um único serviço oAuth (desativa os logins locais!)';
+$lang['singleService_o_']      = 'Permitir todos os serviços';
+$lang['register-on-auth']      = 'Registre usuários autenticados mesmo se o autorregistro estiver desabilitado na configuração principal';
+$lang['overwrite-groups']      = 'Substitua todos os grupos de usuários do DokuWiki por aqueles fornecidos pelo provedor';
