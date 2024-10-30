096e7539236e67acdb0d82d963c1d3e0b7f3c931
Naoto Kobayashi <naoto.kobayashi4c@gmail.com>
2024-10-31 08:43:26 +0900

    Add HttpTokenResponseException making error handling easy
    Although http status code and http error message are helpful in error handling,
    TokenResponseException, throwed by oauth\HTTPClient if an error occured during
    the http request, does not have fields/methods which indicate them directly.
    
    Solve it by adding HttpTokenResponseException which has indicators of http status
    code and http error message, and throwing it instead of TokenResponseException.
    
    Signed-off-by: Naoto Kobayashi <naoto.kobayashi4c@gmail.com>

--- HTTPClient.php
+++ HTTPClient.php
@@ -4,7 +4,6 @@ namespace dokuwiki\plugin\oauth;
 
 use dokuwiki\HTTP\DokuHTTPClient;
 use OAuth\Common\Http\Client\ClientInterface;
-use OAuth\Common\Http\Exception\TokenResponseException;
 use OAuth\Common\Http\Uri\UriInterface;
 
 /**
@@ -26,7 +25,11 @@ class HTTPClient implements ClientInterface
         $ok = $http->sendRequest($endpoint->getAbsoluteUri(), $requestBody, $method);
         if (!$ok || $http->status < 200 || $http->status > 299) {
             $msg = "An error occured during the request to the oauth provider:\n";
-            throw new TokenResponseException($msg . $http->error . ' [HTTP ' . $http->status . ']');
+            throw new HttpTokenResponseException(
+                $msg . $http->error . ' [HTTP ' . $http->status . ']',
+                $http->status,
+                $http->error
+            );
         }
 
         return $http->resp_body;
