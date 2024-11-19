981df0b3a48fb3ae8a05cdcaab437beb5ecb7d67
Thầu xây dựng tại Cam Ranh <tommyhodgkinson@hotmail.com>
2024-11-19 09:20:45 +0100

    translation update

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -1,18 +1,35 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * Slovak language file for oauth plugin
  *
+ * @author Thầu xây dựng tại Cam Ranh <tommyhodgkinson@hotmail.com>
  * @author Viktor Kristian <vkristian@gmail.com>
  */
+$lang['emailduplicate']        = 'tommyhodgkinson@hotmail.com';
+$lang['loginwith']             = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/ 
+Pretty nice post. I just stumbled upon your 
+blog and wanted to say that I\'ve really enjoyed surfing around your blog posts.
 
-$lang['emailduplicate'] = 'Táto e-mailová adresa je už asociovaná s iným používateľom.';
-$lang['loginwith']      = 'Prihlásiť sa pomocou iných služieb:';
-$lang['authnotenabled'] = 'Používateľský účet asociovaný s vašou e-mailovou adresou nemá povolené prihlasovanie sa pomocou %s. Prosím prihláste sa inou metódou, a vo svojom používateľskom profile povoľte prihlasovanie sa pomocou %s.';
-$lang['wrongConfig'] = 'oAuth plugin nie je správne nakonfigurovaný. Je možné prihlásiť sa len pomocou lokálneho účtu. Prosím kontaktujte administrátora stránky.';
-$lang['loginButton'] = 'Prihlásiť sa pomocou ';//... i.e. Google (on SingleAuth)
-$lang['rejectedEMail'] = 'Použitá nesprávna e-mailová adresa. Povolené sú len e-mailové adresy z nasledujúcich domén: %s!';
-$lang['eMailRestricted'] = 'Povolené sú len e-mailové adresy z nasledujcich domén: %s';
-$lang['noEmail'] = 'Služba %s nám neposkytla vašu e-mailovú adresu. Nevieme vás preto prihlásiť.';
-$lang['addUser not possible'] = 'Samoregistrácia užívateľa je momentálne zakázaná, alebo konfigurácia v conf/users.auth.php nie je zapisovateľná. Prosím požiadajte administrátora stránky, aby vám vytvoril váš účet manuálne.';
-$lang['oauth login failed'] = 'Vaše (opätovné) prihlásenie sa zlyhalo.';
-$lang['generic create error'] = 'Počas vytvárania vášho užívateľského účtu niekde nastala chyba. Prosím skúste to znova neskôr.';
+After all I\'ll be subscribing to your rss feed and I hope you write again very soon! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['authnotenabled']        = 'tommyhodgkinson@hotmail.com';
+$lang['wrongConfig']           = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/ 
+Pretty nice post. I just stumbled upon your blog and wanted 
+to say that I\'ve really enjoyed surfing around your blog 
+posts. After all I\'ll be subscribing to your rss feed and I hope you write again very 
+soon! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['loginButton']           = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/ 
+Pretty nice post. I just stumbled upon your blog and wanted to say that 
+I\'ve really enjoyed surfing around your blog posts. After all I\'ll be subscribing to your rss feed and I hope you write again very soon! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['rejectedEMail']         = 'tommyhodgkinson@hotmail.com';
+$lang['eMailRestricted']       = 'tommyhodgkinson@hotmail.com';
+$lang['noEmail']               = 'tommyhodgkinson@hotmail.com';
+$lang['addUser not possible']  = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/ 
+Pretty nice post. I just stumbled upon your blog and wanted to say that I\'ve really 
+enjoyed surfing around your blog posts. After all 
+I\'ll be subscribing to your rss feed and I hope you 
+write again very soon! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['oauth login failed']    = 'Tommy';
+$lang['generic create error']  = 'Tommy';
--- lang/sk/settings.php
+++ lang/sk/settings.php
@@ -1,14 +1,23 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * slovak language file for oauth plugin
  *
+ * @author Thầu xây dựng tại Cam Ranh <tommyhodgkinson@hotmail.com>
  * @author Viktor Kristian <vkristian@gmail.com>
  */
+$lang['info']                  = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['custom-redirectURI']    = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['mailRestriction']       = 'tommyhodgkinson@hotmail.com';
+$lang['singleService']         = 'Tommy';
+$lang['singleService_o_']      = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/ 
+Pretty nice post. I just stumbled upon your blog and wanted to say that I\'ve really enjoyed surfing around your blog posts.
 
-
-$lang['info']               = 'Redirect URI pre konfiguráciu aplikácií';
-$lang['custom-redirectURI'] = 'Použiť vlastné redirect URI';
-$lang['mailRestriction']    = "Obmedziť autentifikáciu na užívateľov z tejto domény (voliteľné, musí začínať s <code>@</code>)";
-$lang['singleService']      = 'Povoliť prihlásenie s jedinou oAuth službou (zakáže prihlasovanie sa s lokálnymi účtami!)';
-$lang['singleService_o_']   = 'Povoliť všetky služby';
-$lang['register-on-auth']   = 'Zaregistrovať autentifikovaných používateľov aj napriek zakázanej samo-registrácii v nastaveniach zabezpečenia';
+After all I\'ll be subscribing to your rss feed and I hope you write again very soon! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['register-on-auth']      = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/ 
+Pretty nice post. I just stumbled upon your blog and wanted to say that I\'ve 
+really enjoyed surfing around your blog posts. After all I\'ll be subscribing 
+to your rss feed and I hope you write again very soon! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-cam-ranh-khanh-hoa-thi-cong-xay-nha-tron-goi/';
+$lang['overwrite-groups']      = 'Tommy';
