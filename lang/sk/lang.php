6968f9e1d150d2cb1c1d2473982b5da94315a84f
Công ty xây dựng Vũng Tàu <shaynaheadley@yahoo.com>
2024-12-20 14:05:37 +0100

    translation update

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -1,18 +1,39 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * Slovak language file for oauth plugin
  *
+ * @author Công ty xây dựng Vũng Tàu <shaynaheadley@yahoo.com>
  * @author Viktor Kristian <vkristian@gmail.com>
  */
+$lang['emailduplicate']        = 'shaynaheadley@yahoo.com';
+$lang['loginwith']             = 'https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Hello There. I discovered your blog the use of msn. This is a 
+very well written article. I will make sure to bookmark it and return to read more 
+of your useful info. Thank you for the post. I\'ll definitely return.
+https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['authnotenabled']        = 'shaynaheadley@yahoo.com';
+$lang['wrongConfig']           = 'https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Hello There. I discovered your blog the use of msn. This is a very well 
+written article. I will make sure to bookmark it and return to read more 
+of your useful info. Thank you for the post. I\'ll definitely return.
+https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['loginButton']           = 'https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Hello There. I discovered your blog the use of msn. This is a very well 
+written article. I will make sure to bookmark it and return to read more of 
+your useful info. Thank you for the post. I\'ll definitely return.
+https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['rejectedEMail']         = 'shaynaheadley@yahoo.com';
+$lang['eMailRestricted']       = 'shaynaheadley@yahoo.com';
+$lang['noEmail']               = 'shaynaheadley@yahoo.com';
+$lang['addUser not possible']  = 'https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/ 
+Hello There. I discovered your blog the use of msn. This is a very well written article.
+I will make sure to bookmark it and return to read more 
+of your useful info. Thank you for the post.
 
-$lang['emailduplicate'] = 'Táto e-mailová adresa je už asociovaná s iným používateľom.';
-$lang['loginwith']      = 'Prihlásiť sa pomocou iných služieb:';
-$lang['authnotenabled'] = 'Používateľský účet asociovaný s vašou e-mailovou adresou nemá povolené prihlasovanie sa pomocou %s. Prosím prihláste sa inou metódou, a vo svojom používateľskom profile povoľte prihlasovanie sa pomocou %s.';
-$lang['wrongConfig'] = 'oAuth plugin nie je správne nakonfigurovaný. Je možné prihlásiť sa len pomocou lokálneho účtu. Prosím kontaktujte administrátora stránky.';
-$lang['loginButton'] = 'Prihlásiť sa pomocou ';//... i.e. Google (on SingleAuth)
-$lang['rejectedEMail'] = 'Použitá nesprávna e-mailová adresa. Povolené sú len e-mailové adresy z nasledujúcich domén: %s!';
-$lang['eMailRestricted'] = 'Povolené sú len e-mailové adresy z nasledujcich domén: %s';
-$lang['noEmail'] = 'Služba %s nám neposkytla vašu e-mailovú adresu. Nevieme vás preto prihlásiť.';
-$lang['addUser not possible'] = 'Samoregistrácia užívateľa je momentálne zakázaná, alebo konfigurácia v conf/users.auth.php nie je zapisovateľná. Prosím požiadajte administrátora stránky, aby vám vytvoril váš účet manuálne.';
-$lang['oauth login failed'] = 'Vaše (opätovné) prihlásenie sa zlyhalo.';
-$lang['generic create error'] = 'Počas vytvárania vášho užívateľského účtu niekde nastala chyba. Prosím skúste to znova neskôr.';
+I\'ll definitely return.
+https://xaydungtrangtrinoithat.com/cong-ty-xay-dung-tai-vung-tau-thi-cong-xay-nha-tron-goi/';
+$lang['oauth login failed']    = 'Shayna';
+$lang['generic create error']  = 'Shayna';
