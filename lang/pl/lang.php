876aa6e070fc81371a9d96b410f1e3a38e9f4aec
Marek Adamski <fevbew@wp.pl>
2025-09-09 15:25:41 +0200

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author dzwdz <dokuwiki@dzwdz.net>
  */
 $lang['emailduplicate']        = 'Ten adres email jest już powiązany z innym użytkownikiem.';
@@ -13,6 +14,8 @@ $lang['wrongConfig']           = 'Plugin oAuth został źle skonfigurowany, moż
 $lang['loginButton']           = 'Zaloguj się poprzez';
 $lang['rejectedEMail']         = 'Użyto niedozwolonego adresu email. Dozwolone są tylko adresy email z następujących domen: %s';
 $lang['eMailRestricted']       = 'Dozwolone są tylko adresy email z następujących domen: %s';
+$lang['eMailEnforced']         = 'Przepraszamy, konta z Twojej domeny muszą się logować przez oAuth.';
 $lang['noEmail']               = 'Serwis %s nie przekazał nam twojego adresu email. Logowanie nie powiodło się.';
+$lang['addUser not possible']  = 'Samodzielna rejestracja jest obecnie wyłączona lub plik conf/users.auth.php nie jest zapisywalny. Poproś administratora DokuWiki o ręczne utworzenie konta.';
 $lang['oauth login failed']    = 'Twoje (ponowne) logowanie nie powiodło się.';
 $lang['generic create error']  = 'Coś poszło nie tak podczas tworzenia twojego konta. Spróbuj ponownie później.';
---
+++ lang/pl/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marek Adamski <fevbew@wp.pl>
+ */
+$lang['info']                  = 'Przekierowanie URI do użycia podczas konfigurowania aplikacji';
+$lang['custom-redirectURI']    = 'Użyj następującego niestandardowego adresu URI przekierowania';
+$lang['mailRestriction']       = 'Ogranicz uwierzytelnianie oAuth do użytkowników z tej domeny (opcjonalne, musi zaczynać się od <code>@</code>). Wiele domen można rozdzielić przecinkami.';
+$lang['mailEnforcement']       = 'Wymuś uwierzytelnianie OAuth dla użytkowników z tej domeny (opcjonalne, musi zaczynać się od <code>@</code>). Wiele domen można rozdzielić przecinkami.';
+$lang['singleService']         = 'Zaloguj się tylko za pomocą pojedynczej usługi OAuth (wyłącza logowanie lokalne!)';
+$lang['singleService_o_']      = 'Zezwól na wszystkie usługi';
+$lang['register-on-auth']      = 'Rejestruj uwierzytelnionych użytkowników, nawet jeśli samodzielna rejestracja jest wyłączona w konfiguracji głównej';
+$lang['overwrite-groups']      = 'Nadpisz wszystkie grupy użytkowników DokuWiki tymi dostarczonymi przez dostawcę';
