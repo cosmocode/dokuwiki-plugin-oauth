59707c5f9394d39ca2834ce4c561dc9f8803f96e
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2025-09-20 17:25:09 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,12 +5,13 @@
  *
  * French language file for oauth plugin
  *
- * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  * @author Cyrille Giquello <cyrille@giquello.fr>
  */
 $lang['info']                  = 'URI de redirection à utiliser dans la configuration de l´application';
 $lang['custom-redirectURI']    = 'Utiliser cet URI de redirection personnalisé';
 $lang['mailRestriction']       = 'Limiter l\'authentification aux utilisateurs de ce domaine (optionnel, doit commencer par un <code>@</code>).';
+$lang['mailEnforcement']       = 'Oblige les utilisateurs de ce domaine (optionnel, doit commencer par un <code>@</code>) à se connecter par oAuth. Séparer plusieurs domaines par des virgules.';
 $lang['singleService']         = 'Se connecter uniquement avec oAuth (désactive les logins locaux)';
 $lang['singleService_o_']      = 'Autoriser tous les services';
 $lang['register-on-auth']      = 'Enregistrer les utilisateurs authentifiés même si l\'enregistrement automatique est désactivé dans la configuration principale';
