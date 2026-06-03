938fdf87892a4dce97ac726257c7e7378e745a64
Wizzard <wizzardsk@gmail.com>
2026-06-03 18:36:16 +0200

    Translation update (sk)

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -1,18 +1,22 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * Slovak language file for oauth plugin
  *
+ * @author Wizzard <wizzardsk@gmail.com>
  * @author Viktor Kristian <vkristian@gmail.com>
  */
-
-$lang['emailduplicate'] = 'Táto e-mailová adresa je už asociovaná s iným používateľom.';
-$lang['loginwith']      = 'Prihlásiť sa pomocou iných služieb:';
-$lang['authnotenabled'] = 'Používateľský účet asociovaný s vašou e-mailovou adresou nemá povolené prihlasovanie sa pomocou %s. Prosím prihláste sa inou metódou, a vo svojom používateľskom profile povoľte prihlasovanie sa pomocou %s.';
-$lang['wrongConfig'] = 'oAuth plugin nie je správne nakonfigurovaný. Je možné prihlásiť sa len pomocou lokálneho účtu. Prosím kontaktujte administrátora stránky.';
-$lang['loginButton'] = 'Prihlásiť sa pomocou ';//... i.e. Google (on SingleAuth)
-$lang['rejectedEMail'] = 'Použitá nesprávna e-mailová adresa. Povolené sú len e-mailové adresy z nasledujúcich domén: %s!';
-$lang['eMailRestricted'] = 'Povolené sú len e-mailové adresy z nasledujcich domén: %s';
-$lang['noEmail'] = 'Služba %s nám neposkytla vašu e-mailovú adresu. Nevieme vás preto prihlásiť.';
-$lang['addUser not possible'] = 'Samoregistrácia užívateľa je momentálne zakázaná, alebo konfigurácia v conf/users.auth.php nie je zapisovateľná. Prosím požiadajte administrátora stránky, aby vám vytvoril váš účet manuálne.';
-$lang['oauth login failed'] = 'Vaše (opätovné) prihlásenie sa zlyhalo.';
-$lang['generic create error'] = 'Počas vytvárania vášho užívateľského účtu niekde nastala chyba. Prosím skúste to znova neskôr.';
+$lang['emailduplicate']        = 'Táto e-mailová adresa je už asociovaná s iným používateľom.';
+$lang['loginwith']             = 'Prihlásiť sa pomocou iných služieb:';
+$lang['authnotenabled']        = 'Používateľský účet asociovaný s vašou e-mailovou adresou nemá povolené prihlasovanie sa pomocou %s. Prosím prihláste sa inou metódou, a vo svojom používateľskom profile povoľte prihlasovanie sa pomocou %s.';
+$lang['wrongConfig']           = 'oAuth plugin nie je správne nakonfigurovaný. Je možné prihlásiť sa len pomocou lokálneho účtu. Prosím kontaktujte administrátora stránky.';
+$lang['loginButton']           = 'Prihlásiť sa pomocou ';
+$lang['rejectedEMail']         = 'Použitá nesprávna e-mailová adresa. Povolené sú len e-mailové adresy z nasledujúcich domén: %s!';
+$lang['eMailRestricted']       = 'Povolené sú len e-mailové adresy z nasledujcich domén: %s';
+$lang['eMailEnforced']         = 'Prepáčte, účty z vašej domény sa musia prihlasovať cez oAuth.';
+$lang['noEmail']               = 'Služba %s nám neposkytla vašu e-mailovú adresu. Nevieme vás preto prihlásiť.';
+$lang['addUser not possible']  = 'Samoregistrácia užívateľa je momentálne zakázaná, alebo konfigurácia v conf/users.auth.php nie je zapisovateľná. Prosím požiadajte administrátora stránky, aby vám vytvoril váš účet manuálne.';
+$lang['oauth login failed']    = 'Vaše (opätovné) prihlásenie sa zlyhalo.';
+$lang['generic create error']  = 'Počas vytvárania vášho užívateľského účtu niekde nastala chyba. Prosím skúste to znova neskôr.';
